49a06b27e5c26797561b2bd6028b621a23934e51
rost21 <rostik.yakimchuk@gmail.com>
2019-01-27 18:06:55 +0200

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,28 +1,197 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from './header/Header';
+import {Dashboard} from './dashboard/Dashboard';
+import {Sidebar} from './sidebar/Sidebar';
 
-class App extends Component {
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000);
+};
+
+const toDoList = [
+  {
+    dashboard_id: generateId(),
+    title: "title01",
+    tasks: [
+      {
+        task_id: generateId(),
+        selected: true,
+        name: "task01",
+      },
+      {
+        task_id: generateId(),
+        selected: false,
+        name: "task02",
+      }
+    ],
+  },
+  {
+    dashboard_id: generateId(),
+    title: "title02",
+    tasks: [
+      {
+        task_id: generateId(),
+        selected: true,
+        name: "task11",
+      }
+    ],
+  }
+];
+
+class Root extends Component {
+
+  generateId = () => {
+    return Math.floor(Math.random() * 1000000);
+  };
+
+  state = {
+    data: toDoList
+  };
+
+  addDashboard = () => {
+
+    let title = document.getElementById('add-title').value;
+    let task = document.getElementById('add-task').value;
+    
+    if (title !== "" && task !== "") {
+      let newDashboard = {
+        dashboard_id: generateId(),
+        title: title,
+        tasks: [
+          {
+            task_id: generateId(),
+            selected: false,
+            name: task,
+          }
+        ],
+      }
+
+    console.log(newDashboard)
+
+    /*this.setState({
+      data: this.state.data.map(item => {
+         console.log(item)
+      })
+    });
+    */
+   this.state.data.push(newDashboard);
+   this.setState(
+      this.state.data
+   );
+
+    //this.state.data.push(newDashboard)
+    console.log(this.state.data)
+    }
+
+  }
+
+  deleteDashboard = (box_id) => {
+    console.log(box_id);
+    this.setState({
+      data: this.state.data.filter(i => i.dashboard_id !== box_id)
+    });
+    console.log(this.state.data);
+  };
+
+  changeDashboardTitle = (box_id, newValue) => {
+    this.setState({
+      data: this.state.data.map(i => 
+        i.dashboard_id === box_id ? { ...i, title: newValue } : i)
+    })
+    console.log(box_id,newValue);
+  };
+
+  addTask = (box_id, e) => {
+    
+    if (e.target.value !== "") {
+
+      let newTask = {
+          task_id: generateId(),
+          selected: false,
+          name: e.target.value,
+      }
+
+      console.log(newTask)
+
+      this.setState({
+        data: this.state.data.map(item =>
+          item.dashboard_id === box_id
+            ? {
+                ...item,
+                tasks: [...item.tasks, newTask]
+              }
+            : item
+        )
+      });
+
+      
+      console.log(this.state.data)
+    }
+
+    e.target.value = "";
+
+  };
+
+  changeTask = (box_id, task_id, newValue) => {
+    this.setState({
+      data: this.state.data.map(i => {
+        if (i.dashboard_id === box_id) {
+          i.tasks.map(task => {
+            if (task.task_id === task_id) {
+              return {...task, name: newValue}
+            }
+            return task;
+          })
+        }
+        return i;
+      })
+    })
+    
+    console.log(task_id,newValue);
+  };
+
+  deleteTask = (box_id, task_id) => {
+    this.setState({
+      data: this.state.data.map(i => {
+          if (i.dashboard_id === box_id) {
+            return {...i, tasks: i.tasks.filter(task => task.task_id !== task_id)}
+          }
+          return i;
+      })
+    });  
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Rekkkkact
-          </a>
-        </header>
+      <div id="block-main">
+          <Header/>
+          <div id="block-content">
+              {this.state.data.map(i => (
+
+                <Dashboard 
+                  key = {i.dashboard_id}
+                  dashboard_id = {i.dashboard_id} 
+                  title = {i.title} 
+                  tasks = {i.tasks} 
+                  deleteDashboard = {this.deleteDashboard}
+                  changeDashboardTitle = {this.changeDashboardTitle}
+                  addTask = {this.addTask}
+                  changeTask = {this.changeTask}
+                  deleteTask = {this.deleteTask}
+                />           
+                ))}
+            
+                         
+          </div>
+             
+        
+        <Sidebar 
+          addDashboard = {this.addDashboard}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export const App = Root;
