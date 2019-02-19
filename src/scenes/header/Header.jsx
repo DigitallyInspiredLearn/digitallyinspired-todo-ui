1db49b48876f1b4f7bf9e32e667b3ab7443d9267
rost21 <rostik.yakimchuk@gmail.com>
2019-02-19 12:15:20 +0200

    lesson/propTypes

--- src/scenes/header/Header.jsx
+++ src/scenes/header/Header.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import './css/headerStyle.css';
+import PropTypes from 'prop-types';
 import logo from '../../image/logo_di.svg';
 
-const Header = () => (
-    <header>
-        <img src={logo} className="logo" alt="logo" />
-        <b>To</b>
-        <p id="line" />
-        <b>do</b>
-    </header>
+
+const Header = ({ children }) => (
+    <div className="App">
+        <header>
+            <img src={logo} className="logo" alt="logo" />
+            <b>To</b>
+            <p id="line" />
+            <b>do</b>
+        </header>
+        {children}
+    </div>
 );
+Header.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+// Header.defaultProps = {
+//     children: undefined,
+// };
+
 
 export default Header;
