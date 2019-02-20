93ecc4636d38860f8c3a8df36ff0de12f1bc45b8
rost21 <rostik.yakimchuk@gmail.com>
2019-02-20 13:24:49 +0200

    start work with settings

--- src/scenes/header/Container.jsx
+++ src/scenes/header/Container.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { compose, bindActionCreators } from 'redux';
 import './css/headerStyle.css';
 import PropTypes from 'prop-types';
+import { actions } from './duck';
 import logo from '../../image/logo_di.svg';
-
+import settings from '../../image/settings.svg';
 
 const Container = ({ children }) => (
     <div className="App">
@@ -11,6 +15,7 @@ const Container = ({ children }) => (
             <b>To</b>
             <p id="line" />
             <b>do</b>
+            <img src={settings} className="settings" alt="setting" />
         </header>
         {children}
     </div>
@@ -19,9 +24,33 @@ Container.propTypes = {
     children: PropTypes.node.isRequired,
 };
 
-// Container.defaultProps = {
-//     children: undefined,
-// };
+const mapStateToProps = state => ({
 
+});
 
-export default Container;
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators({
+        fetchDashboard: actions.fetchDashboard,
+        updateTitleDashboard: actions.updateTitleDashboard,
+        deleteDashboard: actions.deleteDashboard,
+        fetchTasks: actions.fetchTasks,
+        deleteTask: actions.deleteTask,
+        addTask: actions.addTask,
+        updateCheckbox: actions.updateCheckbox,
+        updateTaskName: actions.updateTaskName,
+        updateTitleSuccess: actions.updateTitleSuccess,
+        searchList: actions.searchList,
+        updateSelectedSharedLists: actions.updateSelectedSharedLists,
+        updateSelectedMyLists: actions.updateSelectedMyLists,
+    }, dispatch),
+});
+
+export default compose(
+    withRouter,
+    connect(
+        mapStateToProps,
+        mapDispatchToProps,
+    ),
+)(Container);
+
+// export default Container;
