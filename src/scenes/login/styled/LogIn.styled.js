a749cb07dc3d192c84cfb8f985acd1247f60705a
ketrin996 <bloomingkate789@gmail.com>
2019-02-26 00:37:32 +0200

    Add styled component to authorization and correct registration styles

--- src/scenes/login/styled/LogIn.styled.js
+++ src/scenes/login/styled/LogIn.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const RegistrationStyled = styled.div`
+export const Styled = styled.div`
     margin-top: 20%;
     margin-left: 35%;
     display: flex;
@@ -10,12 +10,12 @@ export const RegistrationStyled = styled.div`
     animation: show 500ms 1;
     animation-fill-mode: forwards;
     animation-delay: 0s;
+    cursor: default;
     
     @keyframes show {
     0%{
         opacity:0;
     }
-
 
     100% {
         opacity:1;
@@ -31,18 +31,18 @@ export const RegistrationStyled = styled.div`
 }
 `;
 
-export const RegistrationNavigationForm = styled.nav`
-     margin-top: 20%;
+export const NavigationForm = styled.nav`
+     margin-bottom: 20%;
      width: 98%;
  `;
 
-export const RegistrationTitle = styled.h2`
+export const Title = styled.h2`
      width: auto;
      margin:15px auto;
      cursor: default;
  `;
 
-export const EnterRegistrationInform = styled.div`
+export const EnterInformation = styled.div`
     position: relative;
     border-bottom: 1px solid gray;
     
@@ -57,22 +57,22 @@ export const EnterRegistrationInform = styled.div`
         transition:  width 0.2s ease-in-out;
         width: 0;
     }
+    
     &:hover:before {
     width: 100%;
     }
 `;
 
-export const RegistrationInput = styled.input`
+export const Input = styled.input`
     outline:none;
     padding: 5px;
-    width: 100%;
+    width: 90%;
     margin: 10px;
  `;
 
-export const RegistrationParagraph = styled.p`
+export const SuccessButton = styled.p`
     outline:none;
-    padding: 5px;
-    width: 100%;
+    width: 85%;
     margin: 10px;
     
     &:hover {
@@ -90,31 +90,28 @@ export const RegistrationParagraph = styled.p`
     background: darkgrey;
     padding: 10px;
     border-radius: 5px;
-    margin-top: 10px;
     border: none;
     cursor: pointer;
  `;
 
-export const RegistrationForm = styled.form`
+export const Form = styled.form`
     margin: 5px;
     padding: 15px;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    
     background: white;
     box-shadow: 0 0  40px 0  rgba(0,0,0,0.2);
     border-radius: 5px;
 `;
 
-export const RegistrationHref = styled.div`
+export const HrefButton = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     margin: 0  5px ;
     padding: 15px;
-    
-     background: white;
+    background: white;
     box-shadow: 0 0  40px 0  rgba(0,0,0,0.2);
     border-radius: 5px;
 `;
