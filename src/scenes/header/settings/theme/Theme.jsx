220f4a2096d5aa2882541d996ff3ab1317aae541
rost21 <rostik.yakimchuk@gmail.com>
2019-03-01 15:01:51 +0200

    theme

--- src/scenes/header/settings/theme/Theme.jsx
+++ src/scenes/header/settings/theme/Theme.jsx
@@ -3,18 +3,82 @@ import * as styled from './Theme.styles';
 import dayImage from '../../../../image/day.png';
 import nightImage from '../../../../image/night.png';
 import paint from '../../../../image/paint.svg';
+import theme from '../../../../config/theme';
 
 class Theme extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            type: props.type,
+            data: props.data,
+        };
     }
 
+    handleDefaultSelect = (type) => {
+        this.setState({ type, data: theme[type] });
+    };
+
+    handleDataChange = (key, value) => {
+        this.setState({
+            type: 'custom',
+            data: {
+                ...this.state.data,
+                [key]: value,
+            },
+        });
+    };
+
+    handleApply = () => {
+        this.props.actions.changeTheme(this.state);
+    };
+
     render() {
-        const { type, actions } = this.props;
+        const { type, data } = this.state;
         return (
             <styled.Theme>
-                <styled.ThemeTitle>Theme selection</styled.ThemeTitle>
+                <styled.ColumnContainer>
+                    <styled.ColumnTitle>Default</styled.ColumnTitle>
+                    <styled.BlockTheme>
+                        <styled.ImageTheme
+                            selected={type === 'day'}
+                            src={dayImage}
+                            onClick={() => this.handleDefaultSelect('day')}
+                        />
+                        <styled.NameTheme>Day theme</styled.NameTheme>
+                    </styled.BlockTheme>
+                    <styled.BlockTheme>
+                        <styled.ImageTheme
+                            selected={type === 'night'}
+                            src={nightImage}
+                            onClick={() => this.handleDefaultSelect('night')}
+                        />
+                        <styled.NameTheme>Night theme</styled.NameTheme>
+                    </styled.BlockTheme>
+                </styled.ColumnContainer>
+                <styled.ColumnContainer>
+                    <styled.ColumnTitle>Properties</styled.ColumnTitle>
+                    <styled.Row>
+                        <styled.Text>Base color:</styled.Text>
+                        <styled.Input
+                            onChange={e => this.handleDataChange('baseColor', e.target.value)}
+                            value={data.baseColor}
+                        />
+
+                    </styled.Row>
+                    <styled.Row>
+                        <styled.Text>Secondary color:</styled.Text>
+                        <styled.Input
+                            onChange={e => this.handleDataChange('secondaryColor', e.target.value)}
+                            value={data.secondaryColor}
+                        />
+                    </styled.Row>
+                    <styled.ApplyButton
+                        onClick={this.handleApply}
+                    >
+                        Apply
+                    </styled.ApplyButton>
+                </styled.ColumnContainer>
+                {/* <styled.ThemeTitle>Theme selection</styled.ThemeTitle>
                 <styled.NewTheme>
                     <styled.BlockTheme>
                         <styled.ImageTheme
@@ -36,7 +100,7 @@ class Theme extends Component {
                 <styled.NewAddingTheme>
                     <styled.PaintTheme src={paint} />
                     <styled.TextTheme>Custom theme</styled.TextTheme>
-                </styled.NewAddingTheme>
+                </styled.NewAddingTheme> */}
             </styled.Theme>
         );
     }
