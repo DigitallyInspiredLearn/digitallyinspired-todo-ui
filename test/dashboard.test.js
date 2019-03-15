2611b471e8726c168b901d0cca27bb1912a00789
ketrin996 <bloomingkate789@gmail.com>
2019-03-15 14:33:47 +0200

    add unit tests to dashboard

--- test/dashboard.test.js
+++ test/dashboard.test.js
@@ -35,4 +35,95 @@ describe('Dashboard test', () => {
         expect(reducer(initialState, action)).toEqual(expected);
         expect(reducer(prevState2, action2)).toEqual(expected2);
     });
+    it('CHANGE_SORT test', () => {
+        const change1 = actions.changeSort('id,desc');
+        const expected1 = {
+            ...initialState,
+            sort: 'id,desc',
+        };
+        const prevSortState = {
+            ...initialState,
+            sort: 'id,desc',
+        };
+        const change2 = actions.changeSort('todoListName,asc');
+        const expected2 = {
+            ...initialState,
+            sort: 'todoListName,asc',
+        };
+        expect(reducer(initialState, change1)).toEqual(expected1);
+        expect(reducer(prevSortState, change2)).toEqual(expected2);
+    });
+    it('SEARCH test', () => {
+        const action = actions.search('kate');
+        const expected = {
+            ...initialState,
+            search: 'kate',
+        };
+        const prevState2 = {
+            ...initialState,
+            search: 'null',
+        };
+        const action2 = actions.search('hello');
+        const expected2 = {
+            ...initialState,
+            search: 'hello',
+        };
+        expect(reducer(initialState, action)).toEqual(expected);
+        expect(reducer(prevState2, action2)).toEqual(expected2);
+    });
+    it('CHANGE_PAGINATION test', () => {
+        const action = actions.changePagination(10);
+        const expected = {
+            ...initialState,
+            currentPage: 10,
+        };
+        const prevState2 = {
+            ...initialState,
+            currentPage: 10,
+        };
+        const action2 = actions.changePagination(7);
+        const expected2 = {
+            ...initialState,
+            currentPage: 7,
+        };
+        expect(reducer(initialState, action)).toEqual(expected);
+        expect(reducer(prevState2, action2)).toEqual(expected2);
+    });
+    it('UPDATE_TASK_Name test', () => {
+        const action = actions.updateTaskName({idTask: 12, newTaskName: 'Task'});
+        const expected = {
+            ...initialState,
+            toDoBoardRaw: {
+                ...initialState.toDoBoardRaw,
+                tasks: initialState.toDoBoardRaw.map(e => (e.id === 12
+                    ? {
+                        ...e, body: 'Task',
+                    } : e)),
+            },
+        };
+        const prevState2 = {
+            ...initialState,
+            toDoBoardRaw: {
+                ...initialState.toDoBoardRaw,
+                tasks: initialState.toDoBoardRaw.map(e => (e.id === 1
+                    ? {
+                        ...e, body: 'Test',
+                    } : e)),
+            },
+        };
+        const action2 = actions.updateTaskName({idTask: 1, newTaskName: 'NewTask'});
+        const expected2 = {
+            ...initialState,
+            toDoBoardRaw: {
+                ...initialState.toDoBoardRaw,
+                tasks: initialState.toDoBoardRaw.map(e => (e.id === 1
+                    ? {
+                        ...e, body: 'NewTask',
+                    } : e)),
+            },
+        };
+        expect(reducer(initialState, action)).toEqual(expected);
+        expect(reducer(prevState2, action2)).toEqual(expected2);
+    });
+});
 });
